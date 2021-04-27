b65ea69be87a539221ade16d8ec78b2607bea9ad
rexim <reximkut@gmail.com>
2021-04-28 02:53:06 +0700

    Rough implementation of the JAM preset

--- index.js
+++ index.js
@@ -89,13 +89,18 @@ void main() {
     "JAM": {
         "vertex": `#version 100
 precision mediump float;
+
 attribute vec2 meshPosition;
+uniform float time;
 
 varying vec2 uv;
 
 void main() {
+    float a = 0.3;
+    float t = (sin(24.0 * time) * a + a) / 2.0;
+
     gl_Position = vec4(
-        meshPosition,
+        meshPosition + vec2(0.0, t),
         0.0,
         1.0);
     uv = (meshPosition + vec2(1.0, 1.0)) / 2.0;
