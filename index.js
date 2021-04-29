1710fdbfeb1ed48bd260bc3d18ea50fa58042586
rexim <reximkut@gmail.com>
2021-04-30 01:35:14 +0700

    Make the Hop filter flip the texture according to the direction

--- index.js
+++ index.js
@@ -137,19 +137,23 @@ float flipping_directions(float time_interval) {
 }
 
 void main() {
-    float scale = 0.25;
+    float scale = 0.40;
     float hops = 2.0;
     float x_time_interval = 0.85;
     float y_time_interval = x_time_interval / (2.0 * hops);
+    float height = 0.5;
     vec2 offset = vec2(
         sliding_from_left_to_right(x_time_interval) * flipping_directions(x_time_interval),
-        ((sliding_from_left_to_right(y_time_interval) * flipping_directions(y_time_interval) + 1.0) / 4.0));
+        ((sliding_from_left_to_right(y_time_interval) * flipping_directions(y_time_interval) + 1.0) / 4.0) - height);
 
     gl_Position = vec4(
         meshPosition * scale + offset,
         0.0,
         1.0);
+
     uv = (meshPosition + vec2(1.0, 1.0)) / 2.0;
+
+    uv.x = (flipping_directions(x_time_interval) + 1.0) / 2.0 - uv.x * flipping_directions(x_time_interval);
 }
 `,
         "fragment": `#version 100
@@ -357,7 +361,7 @@ window.onload = () => {
         gl.uniform1f(program.timeUniform, start * 0.001);
         gl.uniform2f(program.resolutionUniform, canvas.width, canvas.height);
 
-        gl.clearColor(0.0, 0.0, 0.0, 0.0);
+        gl.clearColor(0.0, 1.0, 0.0, 1.0);
         gl.clear(gl.COLOR_BUFFER_BIT);
 
         gl.drawArrays(gl.TRIANGLES, 0, TRIANGLE_PAIR * TRIANGLE_VERTICIES);
