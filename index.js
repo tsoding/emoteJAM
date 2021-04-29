d35564d91b37d5cfa57fa6cdedcd36de10bae16a
rexim <reximkut@gmail.com>
2021-04-30 05:35:51 +0700

    Rename "Clowning" to "Blob"

--- index.js
+++ index.js
@@ -41,28 +41,42 @@ function linkShaderProgram(gl, shaders, vertexAttribs) {
 }
 
 const presets = {
-    "Clowning": {
-        "duration": Math.PI / 3,
+    "Hop": {
+        "duration": 0.85 * 2,
         "vertex": `#version 100
-
 precision mediump float;
 
 attribute vec2 meshPosition;
-
-uniform vec2 resolution;
 uniform float time;
 
 varying vec2 uv;
 
-void main() {
-    float stretch = sin(6.0 * time) * 0.5 + 1.0;
+float sliding_from_left_to_right(float time_interval) {
+    return (mod(time, time_interval) - time_interval * 0.5) / (time_interval * 0.5);
+}
 
-    vec2 offset = vec2(0.0, 1.0 - stretch);
+float flipping_directions(float time_interval) {
+    return 1.0 - 2.0 * mod(floor(time / time_interval), 2.0);
+}
+
+void main() {
+    float scale = 0.40;
+    float hops = 2.0;
+    float x_time_interval = 0.85;
+    float y_time_interval = x_time_interval / (2.0 * hops);
+    float height = 0.5;
+    vec2 offset = vec2(
+        sliding_from_left_to_right(x_time_interval) * flipping_directions(x_time_interval) * (1.0 - scale),
+        ((sliding_from_left_to_right(y_time_interval) * flipping_directions(y_time_interval) + 1.0) / 4.0) - height);
+
     gl_Position = vec4(
-        meshPosition * vec2(stretch, 2.0 - stretch) + offset,
+        meshPosition * scale + offset,
         0.0,
         1.0);
+
     uv = (meshPosition + vec2(1.0, 1.0)) / 2.0;
+
+    uv.x = (flipping_directions(x_time_interval) + 1.0) / 2.0 - uv.x * flipping_directions(x_time_interval);
 }
 `,
         "fragment": `#version 100
@@ -120,42 +134,28 @@ void main() {
 }
 `
     },
-    "Hop": {
-        "duration": 0.85 * 2,
+    "Blob": {
+        "duration": Math.PI / 3,
         "vertex": `#version 100
+
 precision mediump float;
 
 attribute vec2 meshPosition;
+
+uniform vec2 resolution;
 uniform float time;
 
 varying vec2 uv;
 
-float sliding_from_left_to_right(float time_interval) {
-    return (mod(time, time_interval) - time_interval * 0.5) / (time_interval * 0.5);
-}
-
-float flipping_directions(float time_interval) {
-    return 1.0 - 2.0 * mod(floor(time / time_interval), 2.0);
-}
-
 void main() {
-    float scale = 0.40;
-    float hops = 2.0;
-    float x_time_interval = 0.85;
-    float y_time_interval = x_time_interval / (2.0 * hops);
-    float height = 0.5;
-    vec2 offset = vec2(
-        sliding_from_left_to_right(x_time_interval) * flipping_directions(x_time_interval) * (1.0 - scale),
-        ((sliding_from_left_to_right(y_time_interval) * flipping_directions(y_time_interval) + 1.0) / 4.0) - height);
+    float stretch = sin(6.0 * time) * 0.5 + 1.0;
 
+    vec2 offset = vec2(0.0, 1.0 - stretch);
     gl_Position = vec4(
-        meshPosition * scale + offset,
+        meshPosition * vec2(stretch, 2.0 - stretch) + offset,
         0.0,
         1.0);
-
     uv = (meshPosition + vec2(1.0, 1.0)) / 2.0;
-
-    uv.x = (flipping_directions(x_time_interval) + 1.0) / 2.0 - uv.x * flipping_directions(x_time_interval);
 }
 `,
         "fragment": `#version 100
@@ -174,7 +174,7 @@ void main() {
     gl_FragColor.w = floor(gl_FragColor.w + 0.5);
 }
 `
-    }
+    },
 };
 
 function createTextureFromImage(gl, image) {
