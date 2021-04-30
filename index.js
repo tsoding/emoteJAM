a78ce3534d384c5f991251b7fe5250e670b9ee14
rexim <reximkut@gmail.com>
2021-05-01 03:00:21 +0700

    Implement Slide filter

--- index.js
+++ index.js
@@ -295,6 +295,61 @@ void main() {
 }
 `,
     },
+    "Slide": {
+        "duration": 0.85 * 2,
+        "vertex": `#version 100
+precision mediump float;
+
+attribute vec2 meshPosition;
+uniform float time;
+
+varying vec2 uv;
+
+float sliding_from_left_to_right(float time_interval) {
+    return (mod(time, time_interval) - time_interval * 0.5) / (time_interval * 0.5);
+}
+
+float flipping_directions(float time_interval) {
+    return 1.0 - 2.0 * mod(floor(time / time_interval), 2.0);
+}
+
+void main() {
+    float scale = 0.40;
+    float hops = 2.0;
+    float x_time_interval = 0.85;
+    float y_time_interval = x_time_interval / (2.0 * hops);
+    float height = 0.5;
+    vec2 offset = vec2(
+        sliding_from_left_to_right(x_time_interval) * flipping_directions(x_time_interval) * (1.0 - scale),
+        - height);
+
+    gl_Position = vec4(
+        meshPosition * scale + offset,
+        0.0,
+        1.0);
+
+    uv = (meshPosition + vec2(1.0, 1.0)) / 2.0;
+
+    uv.x = (flipping_directions(x_time_interval) + 1.0) / 2.0 - uv.x * flipping_directions(x_time_interval);
+}
+`,
+        "fragment": `#version 100
+
+precision mediump float;
+
+uniform vec2 resolution;
+uniform float time;
+
+uniform sampler2D emote;
+
+varying vec2 uv;
+
+void main() {
+    gl_FragColor = texture2D(emote, vec2(uv.x, 1.0 - uv.y));
+    gl_FragColor.w = floor(gl_FragColor.w + 0.5);
+}
+`
+    },
     "Laughing": {
         "duration": Math.PI / 12.0,
         "vertex": `#version 100
