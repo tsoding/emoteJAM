0113dd0bb48a5181d920b0fd7bcf08238bfbdd58
rexim <reximkut@gmail.com>
2021-05-01 02:57:36 +0700

    Implement Circle Filter

--- index.js
+++ index.js
@@ -244,6 +244,57 @@ void main() {
 }
 `,
     },
+    "Circle": {
+        "duration": Math.PI / 4.0,
+        "vertex": `#version 100
+precision mediump float;
+
+attribute vec2 meshPosition;
+
+uniform vec2 resolution;
+uniform float time;
+
+varying vec2 uv;
+
+vec2 rotate(vec2 v, float a) {
+	float s = sin(a);
+	float c = cos(a);
+	mat2 m = mat2(c, -s, s, c);
+	return m * v;
+}
+
+void main() {
+    float scale = 0.30;
+    float period_interval = 8.0;
+    float pi = 3.141592653589793238;
+    vec2 outer_circle = vec2(cos(period_interval * time), sin(period_interval * time)) * (1.0 - scale);
+    vec2 inner_circle = rotate(meshPosition * scale, (-period_interval * time) + pi / 2.0);
+    gl_Position = vec4(
+        inner_circle + outer_circle,
+        0.0,
+        1.0);
+    uv = (meshPosition + 1.0) / 2.0;
+}
+`,
+        "fragment": `
+#version 100
+
+precision mediump float;
+
+uniform vec2 resolution;
+uniform float time;
+
+uniform sampler2D emote;
+
+varying vec2 uv;
+
+void main() {
+    float speed = 1.0;
+    gl_FragColor = texture2D(emote, vec2(uv.x, 1.0 - uv.y));
+    gl_FragColor.w = floor(gl_FragColor.w + 0.5);
+}
+`,
+    },
     "Laughing": {
         "duration": Math.PI / 12.0,
         "vertex": `#version 100
