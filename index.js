2294b09c808260ec90bab6ff8fd4b12ae98dae69
coppercasted <coppercasted@googlemail.com>
2021-05-03 12:59:18 +0100

    Modified the peek effect so that it loops.

--- index.js
+++ index.js
@@ -653,7 +653,7 @@ void main() {
     },
 	"Peek":{
         "transparent": 0x00FF00,
-        "duration": 2.0 * Math.PI ,
+        "duration": 5.0 * Math.PI ,
         "vertex": `#version 100
 precision mediump float;
 
@@ -665,9 +665,18 @@ uniform float time;
 varying vec2 uv;
 
 void main() {
-    float time_clipped = clamp(mod(time, 3.14), 0.0, 1.57);
+    float time_clipped= mod(time, (4.0 * 3.14));
 
-    gl_Position = vec4(meshPosition.x + cos(time_clipped) + 1.0 , meshPosition.y, 0.0, 1.0);
+    float s1 = float(time_clipped < (2.0 * 3.14));
+    float s2 = 1.0 - s1;
+
+    float hold1 = float(time_clipped > (0.5 * 3.14) && time_clipped < (2.0 * 3.14));
+    float hold2 = 1.0 - float(time_clipped > (2.5 * 3.14) && time_clipped < (4.0 * 3.14));
+
+    float cycle_1 = 1.0 - ((s1 * sin(time_clipped) * (1.0 - hold1)) + hold1);
+    float cycle_2 = s2 * hold2 * (sin(time_clipped) - 1.0); 
+
+    gl_Position = vec4(meshPosition.x + 1.0 + cycle_1 + cycle_2 , meshPosition.y, 0.0, 1.0);
     uv = (meshPosition + 1.0) / 2.0;
 }
 `,
