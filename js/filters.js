40ac42e02b41a5d5aa93ea7e6f39e289cfeb8ab5
rexim <reximkut@gmail.com>
2021-06-08 02:50:21 +0700

    Additional work

--- js/filters.js
+++ js/filters.js
@@ -4,6 +4,7 @@ const filters = {
     "Hop": {
         "transparent": 0x00FF00,
         "duration": "interval * 2",
+        // TODO: when you have too many params the UI gets really cluttered
         "params": {
             "interval": {
                 "type": "float",
