95998191f67fe1a4ace4203af558cedaaf82db74
rexim <reximkut@gmail.com>
2021-06-15 20:20:35 +0700

    Add TODO(#73)

--- ts/grecha.ts
+++ ts/grecha.ts
@@ -1,7 +1,7 @@
 const LOREM: string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
 type Child = string | HTMLElement;
-// TODO: make tag more typesafe
+// TODO(#73): make tag more typesafe
 // Essentially get rid of the `any`
 type Tag = any;
 
